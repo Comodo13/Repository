5528207ae11cdd8e9bcec5e4b80a3ad69872c6c7
Comodo13 <dimanazdar@gmail.com>
2021-01-12 17:19:14 +0100

    1ST Commit Basic strucure

---
+++ src/main/java/net/bookmanager/Baseclass.java
@@ -0,0 +1,22 @@
+package net.bookmanager;
+
+public class Baseclass {
+    int go = 110;
+    Double lonly = 1203.1;
+
+    public int getGo() {
+        return go;
+    }
+
+    public void setGo(int go) {
+        this.go = go;
+    }
+
+    public Double getLonly() {
+        return lonly;
+    }
+
+    public void setLonly(Double lonly) {
+        this.lonly = lonly;
+    }
+}
---
+++ src/main/java/net/bookmanager/HelloController.java
@@ -0,0 +1,4 @@
+package net.bookmanager;
+
+public class HelloController {
+}
